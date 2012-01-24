03a68bc3834c92aafa0b75aa184e7e62df96f3be
William Strecker-Kellogg <willsk@bnl.gov>
2012-01-24 16:03:05 -0500

    Users and auto_regroup -> accept_surplus

--- group_table.sql
+++ group_table.sql
@@ -7,9 +7,17 @@ CREATE TABLE `atlas_group_quotas` (
       `group_name` varchar(64) NOT NULL,
       `quota` int(12) NOT NULL DEFAULT 0,
       `priority` double DEFAULT 10.0,
-      `auto_regroup` boolean DEFAULT False,
+      `accept_surplus` boolean DEFAULT False,
       `busy` int(12),
       `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
       PRIMARY KEY (`group_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+CREATE USER 'atlas_update'@'%' IDENTIFIED BY 'xxx';
+CREATE USER 'atlas_edit'@'%' IDENTIFIED BY 'xxx';
+
+GRANT SELECT, UPDATE ON linux_farm.atlas_group_quotas TO 'atlas_update'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON linux_farm.atlas_group_quotas TO 'atlas_edit'@'%';
+
+FLUSH PRIVILEGES;
