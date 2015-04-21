669bcafca919781fa19597f4028072dd48aa5189
William Strecker-Kellogg <willsk@bnl.gov>
2015-04-21 16:51:51 -0400

    Update DB schema

--- group_table.sql
+++ group_table.sql
@@ -6,13 +6,24 @@ DROP TABLE IF EXISTS `atlas_group_quotas`;
 CREATE TABLE `atlas_group_quotas` (
       `group_name` varchar(128) NOT NULL,
       `quota` int(12) NOT NULL DEFAULT 0,
-      `priority` double DEFAULT 10.0,
+      `priority` double NOT NULL DEFAULT 10.0,
+      `weight` double NOT NULL DEFAULT 1.0,
       `accept_surplus` boolean DEFAULT False,
       `busy` int(12) NOT NULL DEFAULT 0,
+      `surplus_threshold` int(10) unsigned NOT NULL DEFAULT 0,
       `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      `last_surplus_update` timestamp NULL DEFAULT NULL,
       PRIMARY KEY (`group_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `queue_log` (
+  `group_name` varchar(128) NOT NULL DEFAULT '',
+  `amount_in_queue` int(10) unsigned NOT NULL DEFAULT '0',
+  `query_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `ts_idx` (`query_time`),
+  KEY `group_name_idx` (`group_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE USER 'atlas_update'@'%' IDENTIFIED BY 'xxx';
 CREATE USER 'atlas_edit'@'%' IDENTIFIED BY 'xxx';
