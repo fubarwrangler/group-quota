6a2617c9cbf74bca559aef3643d9ecc66fd2d284
William Strecker-Kellogg <willsk@bnl.gov>
2015-05-29 17:00:05 -0400

    Unused config parameters

--- QBalance/config/__init__.py
+++ QBalance/config/__init__.py
@@ -5,26 +5,9 @@
 db = {
     'host': 'localhost',
     'db': 'group_quotas',
-    'port': 3333,
     'passwd': 'CHANGEME',
     'user': 'gqu'
 }
-
-dbtable = 'atlas_group_quotas'
-queue_log_table = 'atlas_queue_log'
-
-# DB Fields
-accept_surplus = 'accept_surplus'
-amount_in_queue = 'amount_in_queue'
-busy = 'busy'
-group_name = 'group_name'
-last_surplus_update = 'last_surplus_update'
-last_update = 'last_update'
-query_time = 'query_time'
-quota = 'quota'
-threshold = 'surplus_threshold'
-weight = 'weight'
-
 
 # Minutes to look back for last change
 lookback = 60
