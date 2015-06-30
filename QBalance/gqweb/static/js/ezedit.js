2376dc3722dbd8472555e298d349ea46d2245ba0
William Strecker-Kellogg <willsk@bnl.gov>
2015-06-30 15:41:38 -0400

    Over/under not working -- will try proportional

--- QBalance/gqweb/static/js/ezedit.js
+++ QBalance/gqweb/static/js/ezedit.js
@@ -12,7 +12,15 @@ function update_quotadisp() {
         sum += this.valueAsNumber;
     });
     $('#debugsum').text(sum);
-};
+}
+
+function jqsumval($elements)  {
+    var sum = 0;
+    $elements.each(function() {
+        sum += this.valueAsNumber;
+    });
+    return sum;
+}
 
 function total_quota() {
     $('.tqdisp').text($('#totalQuota').text());
@@ -27,15 +35,42 @@ $sliders.on('input', function(event) {
     if(this.oldval === this.valueAsNumber) {
         return;
     }
-    var diff = this.oldval - this.valueAsNumber;
-    console.log(this.name, this.oldval, this.valueAsNumber, diff);
-    $sliders.not(this).each(function() {
-        var change = diff / $sliders.length;
-        // console.log($('#' + this.name + "+disp_quota"));
-        console.log("Adjust", this.name, 'by', change )
-        this.valueAsNumber += (diff / ($sliders.length - 1));
+
+    var change = this.oldval - this.valueAsNumber;
+    var len = $sliders.length - 1;
+    var extra = 0;
+
+    console.log(this.name, this.oldval, this.valueAsNumber, change);
+
+    var $othersliders = $sliders.not(this);
+
+    $othersliders.each(function() {
+        var max = Number(this.max);
+        var diff = (change + extra) / len;
+
+        newval = this.valueAsNumber + diff;
+        console.log("..Adjust", this.name, 'by', diff, ':',
+                    this.valueAsNumber, '-->', newval);
+
+        // If would over/under-shoot
+        if (newval > max)  {
+            extra += newval - max;
+            console.log("....too large, extra for next is", extra);
+            this.valueAsNumber = max;
+        } else if (newval < 0) {
+            extra = newval - this.valueAsNumber;
+            console.log("....too small, extra for next is", extra);
+            this.valueAsNumber = 0;
+        } else {
+            extra = 0;
+            this.valueAsNumber = newval;
+        }
+
+        // this.valueAsNumber += (diff / ($sliders.length - 1));
         this.oldval = this.valueAsNumber;
+
     });
+    var othersum = jqsumval($sliders);
     this.oldval = this.valueAsNumber;
     update_quotadisp();
 });
