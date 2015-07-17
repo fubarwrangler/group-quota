67fe74ac7ce1c88e1285f3ed2677bf3ae13a7da7
William Strecker-Kellogg <willsk@bnl.gov>
2015-07-17 16:20:15 -0400

    Tree-chooser working well now

---
+++ QBalance/gqweb/static/js/treeview.js
@@ -0,0 +1,22 @@
+/* Toggle correct classes for highlight event on hover-over link */
+$('div.parent').hover(function(event)    {
+    $(this).children('span').toggleClass('fnt-bold').toggleClass('text-muted');
+    $(this.nextElementSibling).find('div.treediv').andSelf().toggleClass('sel');
+    $(this.nextElementSibling).find('div:not(.treediv)').toggleClass('childedit');
+    $(this.nextElementSibling).children('div.nonparent,div.parent').toggleClass('willedit').toggleClass('childedit');
+    $(this).toggleClass('editparent');
+    $('#colorkey').toggleClass('noseeme');
+});
+
+
+/* Equalize the widths of the divs on the same level for the tree on pageload */
+$(function()    {
+    $('div.treediv').each(function() {
+        var $direct_children = $(this).children('.parent,.nonparent');
+        var maxw = 0;
+        $direct_children.each(function(){
+            if ($(this).width() > maxw) { maxw = $(this).width(); }
+        });
+        $direct_children.width(maxw + 20);
+    });
+});
