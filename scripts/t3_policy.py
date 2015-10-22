66c37dfdb22bc9719867aedc09fd064784344e44
William Strecker-Kellogg <willsk@bnl.gov>
2015-10-22 17:51:18 -0400

    Work on script to autogenerate startd config

---
+++ scripts/t3_policy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import MySQLdb
+from collections import defaultdict
+
+DBHOST = "localhost"
+DBUSER = "willsk"
+DB = "t3groups"
+
+
+def do_query():
+    query = """
+    SELECT u.name, i.group FROM t3users AS u
+    JOIN institutes AS i ON u.affiliation=i.name
+    """
+
+    con = MySQLdb.connect(host=DBHOST, user=DBUSER, db=DB)
+    cur = con.cursor()
+    cur.execute(query)
+    results = cur.fetchall()
+    con.close()
+    return results
+
+groups = defaultdict(list)
+
+for name, group in do_query():
+    groups[group].append(name)
+
+template = """
+VALID_MEMBER = \
+$(VALID_MEMBER) || \
+( ( (TARGET.AcctGroup == "{group}") || (TARGET.AcctGroup == "{group}.long") \
+  ) && stringListMember(Owner, "{namelist}") )
+
+"""
+
+for group, userlist in groups.iteritems():
+    print template.format(group=group, namelist=",".join(userlist))
