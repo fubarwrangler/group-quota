36fe538818bb6b63c87a9f2ab0283c6b403c8aaa
William Strecker-Kellogg <willsk@bnl.gov>
2015-10-26 14:15:17 -0400

    T3 Condor Policy generation

--- scripts/t3_policy.py
+++ scripts/t3_policy.py
@@ -13,7 +13,6 @@ def do_query():
     SELECT u.name, i.group FROM t3users AS u
     JOIN institutes AS i ON u.affiliation=i.name
     """
-
     con = MySQLdb.connect(host=DBHOST, user=DBUSER, db=DB)
     cur = con.cursor()
     cur.execute(query)
@@ -27,12 +26,14 @@ for name, group in do_query():
     groups[group].append(name)
 
 template = """
-VALID_MEMBER = \
-$(VALID_MEMBER) || \
-( ( (TARGET.AcctGroup == "{group}") || (TARGET.AcctGroup == "{group}.long") \
-  ) && stringListMember(Owner, "{namelist}") )
-
+# Policy for {group}
+VALID_MEMBER = $(VALID_MEMBER) || \\
+( ( (TARGET.AcctGroup == "{group}") || (TARGET.AcctGroup == "{group}.long") ) && \\
+  stringListMember(Owner, "{namelist}") )
 """
 
 for group, userlist in groups.iteritems():
+    print "# =============================================="
+    print "# Condor Policy for Validating Group Memberships"
+    print "# ==============================================\n"
     print template.format(group=group, namelist=",".join(userlist))
