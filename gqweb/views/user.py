78ca0dce1f488910e4d76371596e428a3353dc6b
William Strecker-Kellogg <willsk@bnl.gov>
2016-03-01 17:00:29 -0500

    Work on better algorithm for finding covered children

--- gqweb/views/user.py
+++ gqweb/views/user.py
@@ -132,14 +132,28 @@ def user_group_view(u):
     tree = build_group_tree_db(Group.query.all())
     children = defaultdict(list)
     user_groups = set(x.group_name for x in user.groups)
+    available = set()
 
-    for group in tree:
-        for ug in user_groups:
-            if group.full_name.startswith(ug) and ug != group.full_name:
-                children[ug].append(group.full_name)
+    for group in tree.breadth_first():
+        if getattr(group, 'visited', False):
+            continue
+        group.visited = True
+        if group.full_name in user_groups:
+            for child in group:
+                child.visited = True
+        else:
+            available.add(group.full_name)
 
-    can_add = [x.full_name for x in tree if not any(x.full_name.startswith(y) for y in user_groups)]
-    app.logger.info("%s :::: %s", children, can_add)
+    app.logger.info(available)
+    can_add = []
+
+    # for group in tree:
+    #     for ug in user_groups:
+    #         if group.full_name.startswith(ug) and ug != group.full_name:
+    #             children[ug].append(group.full_name)
+    #
+    # can_add = [x.full_name for x in tree if not any(x.full_name.startswith(y) for y in user_groups)]
+    # app.logger.info("%s :::: %s", children, can_add)
 
     return render_template('user_groupedit.html', u=user, groups=tree, children=children, avail=can_add)
 
