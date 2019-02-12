b69774cd4717b3a5a76bfe75d7dd63c2c94f4d3f
William Strecker-Kellogg <will.streckerkellogg@gmail.com>
2019-02-12 14:14:34 -0500

    Script now generates usermap on intercative hosts

--- scripts/t3_policy.py
+++ scripts/t3_policy.py
@@ -12,14 +12,12 @@ import subprocess
 
 import MySQLdb
 
-from collections import defaultdict
-
 DBHOST = "rcfdb1.rcf.bnl.gov"
 DBUSER = "db_query"
 DB = "t3_groups"
 
 TMP_FILE = '/etc/condor/tmpPolicyConfig'
-CONFIG_PATH = '/etc/condor/config.d/50_valid_member'
+CONFIG_PATH = '/etc/condor/tier3.usermap'
 
 logging.basicConfig(filename='/tmp/htcondor-policy.log', level=logging.INFO,
                     format="%(asctime)-15s (%(levelname)-7s) %(message)s")
@@ -28,45 +26,27 @@ log = logging.getLogger()
 
 def get_group_data():
 
-    def do_query():
-        query = """
-        SELECT u.name, i.group FROM t3users AS u
-        JOIN institutes AS i ON u.affiliation=i.name
-        """
-        con = MySQLdb.connect(host=DBHOST, user=DBUSER, db=DB, connect_timeout=20)
-        cur = con.cursor()
-        cur.execute(query)
-        results = cur.fetchall()
-        con.close()
-        return results
-
-    groups = defaultdict(list)
-
-    skip = ['group_atlas.general', 'group_atlas.panda']
-
-    for name, group in do_query():
-        if group not in skip:
-            groups[group].append(name)
-
-    return groups
+    query = """
+    SELECT u.name, i.group FROM t3users AS u
+    JOIN institutes AS i ON u.affiliation=i.name
+    """
+    con = MySQLdb.connect(host=DBHOST, user=DBUSER, db=DB, connect_timeout=20)
+    cur = con.cursor()
+    cur.execute(query)
+    results = cur.fetchall()
+    con.close()
+    return results
 
 
 def write_file(path, data):
-
-    template = """
-# Policy for {group}
-VALID_MEMBER = $(VALID_MEMBER) || \\
-( ( (TARGET.AcctGroup == "{group}") || (TARGET.AcctGroup == "{group}.long") ) && \\
-  stringListMember(TARGET.Owner, "{namelist}") )
-"""
     with open(path, 'w') as fp:
         print >> fp, "# =============================================="
-        print >> fp, "# Condor Policy for Validating Group Memberships"
+        print >> fp, "# Policy map for Tier-3 Institution membership"
         print >> fp, "# ==============================================\n"
-        print >> fp, 'VALID_MEMBER = (TARGET.AcctGroup == "group_atlas.general")\n'
 
-        for group, userlist in data.iteritems():
-            print >> fp, template.format(group=group, namelist=",".join(userlist))
+        for user, group in data:
+            print >> fp, "* %s %s" % (user, group)
+        print >> fp, "* /.*/ group_atlas.tier3.general\n"
 
 
 def hash_file(path):
@@ -96,5 +76,5 @@ def do_main():
 if __name__ == "__main__":
     try:
         do_main()
-    except:
+    except Exception:
         log.exception("Error occured")
